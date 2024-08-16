2b5b3a723be11830bd0f20d488b0cbbc98bc7523
NahiyaNasir <nahiaesha.01@gmail.com>
2024-08-16 16:07:14 +0600

    basic-server-setup

---
+++ index.js
@@ -0,0 +1,67 @@
+
+
+require("dotenv").config();
+const express = require("express");
+
+
+const app = express();
+
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+//   middle ware
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send(" Shop Hive is running  ");
+  });
+
+  
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gze7wpc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+ 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    const userCollection = client.db("shopHive").collection("users");
+    const productCollection=client.db("shopHive").collection('products')
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+
+      //   users api
+    // post users
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        // console.log(user)
+        const query = { email: user.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: " user already exits" });
+        }
+     
+        const result = await userCollection.insertOne(user);
+        // console.log(result);
+        res.send(result);
+      });
+      //   get
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.listen(port, () => {
+    console.log(`Shop Hive is running at ${port}`);
+  });
+  
